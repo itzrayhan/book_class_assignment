b4ad45ee147aedbf40f46c349e478aad87bdf8e6
Rayhan <itzrayhan00@gmail.com>
2024-09-28 00:16:45 +0600

    change

--- lib/bookClass.dart
+++ lib/bookClass.dart
@@ -1,40 +1,9 @@
-class Book {
-  // Properties
-  String title;
-  String author;
-  int publicationYear;
-  int pagesRead = 0;
-  static int totalBooks = 0;
-
-  // Constructor
-  Book(this.title, this.author, this.publicationYear) {
-    totalBooks++; // Increment totalBooks whenever a new Book object is created
-  }
-
-  // Method to read a specified number of pages
-  void read(int pages) {
-    pagesRead += pages;
-  }
-
-  // Getter methods
-  int getPagesRead() {
-    return pagesRead;
-  }
-
-  String getTitle() {
-    return title;
-  }
-
-  String getAuthor() {
-    return author;
-  }
-
-  int getPublicationYear() {
-    return publicationYear;
-  }
-
-  // Method to calculate the age of the book
-  int getBookAge() {
-    return DateTime.now().year - publicationYear;
+class Car{
+  String brand;
+  String model;
+  int year;
+  Car(this.brand, this.model, this.year);
+  int carAge(){
+    return DateTime.now().year-year;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,11 @@
-import 'bookClass.dart';
+import 'carClass.dart';
+void main(){
+  Car myCar=Car('Hunda', 'CR-V ', 2015);
 
-void main() {
-  // Creating three Book objects
-  Book book1 = Book("হাবলুদের জন্য প্রোগ্রামিং", "ঝংকার মাহবুব", 2016);
-  Book book2 = Book("প্রোগ্রামিং কনটেস্ট সমস্যা ও সমাধান", "মো: মাহবুবুল হাসান", 2012);
-  Book book3 = Book("রিচার্জ ইয়োর ডাউন ব্যাটারি", "ঝংকার মাহবুব", 2010);
-
-  // Simulating reading pages
-  book1.read(50);
-  book2.read(30);
-  book3.read(70);
-
-  // Printing details for each book
-  List<Book> books = [book1, book2, book3];
-
-  for (var book in books) {
-    print("Title: ${book.getTitle()}");
-    print("Author: ${book.getAuthor()}");
-    print("Publication Year: ${book.getPublicationYear()}");
-    print("Pages Read: ${book.getPagesRead()}");
-    print("Book Age: ${book.getBookAge()} years\n");
-  }
-
-  // Displaying total number of books created
-  print("Total Books Created: ${Book.totalBooks}");
+  print('Brand : ${myCar.brand}');
+  print('Model : ${myCar.model}');
+  print('Year : ${myCar.year}');
+  print('Car age : ${myCar.carAge()} years');
 }
-k
+
+
